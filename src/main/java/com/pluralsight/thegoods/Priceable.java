4fdfe3b5d1776a8cdbce58856b7973238f53c297
Jeremy <jeremybaez919@gmail.com>
2026-05-22 16:05:47 -0700

    Diagram made along with board. Starting Product parent class and

---
+++ src/main/java/com/pluralsight/thegoods/Priceable.java
@@ -0,0 +1,8 @@
+package com.pluralsight.thegoods;
+
+// any product with a price, must be defined by getPrice()
+public interface Priceable {
+
+    double getPrice();
+
+}
---
+++ src/main/java/com/pluralsight/thegoods/Product.java
@@ -0,0 +1,9 @@
+package com.pluralsight.thegoods;
+
+// abstract parent class
+
+public abstract class Product
+    implements Priceable {
+
+    }
+
