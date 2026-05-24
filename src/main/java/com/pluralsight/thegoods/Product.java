ada13de1f68639d6444d89e881a7a805e352555e
Jeremy <jeremybaez919@gmail.com>
2026-05-23 18:15:43 -0700

    Product class complete

--- src/main/java/com/pluralsight/thegoods/Product.java
+++ src/main/java/com/pluralsight/thegoods/Product.java
@@ -5,5 +5,34 @@ package com.pluralsight.thegoods;
 public abstract class Product
     implements Priceable {
 
+    //shared name for all products
+    private String name;
+
+    //constructor
+    public Product(String name) {
+
+        this.name = name;
+
     }
+
+    //getter for name
+    public String getName() {
+        return name;
+
+    }
+
+    //setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //abstract method: child classes define their own logic for calculating price
+    @Override
+    public abstract double getPrice();
+
+    @Override
+    public String toString() {
+        return "Product: " + name + ", price: " + getPrice();
+    }
+}
 
